06f7ef772ed3c444cb33c52947ed428f1e0a2161
Yamkela <owamjames1@gmail.com>
2017-09-08 16:06:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//start the server
+var server = app.listen(3000);
