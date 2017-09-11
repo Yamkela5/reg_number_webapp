24c04125687b9ec296df9187f4e63e538f2107bd
Yamkela <owamjames1@gmail.com>
2017-09-11 16:20:31 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser')
+var exphbs  = require('express-handlebars');
 app.use(express.static('public'));
+//empty array that I will push reg numbers to
+var regNumberList=[];
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
 
+//create a get router that take a Registration number
 
+app.get('/',function(req, res){
+  var name = req.body.name;
+  res.render('index')
+})
 
+app.post('/reg_number',function(req, res){
 
+var name = req.body.name;
 
+regNumberList.push(name)
+
+  res.render('index',{
+    numplate: regNumberList
+
+  })
+})
 
 
 
@@ -28,4 +51,11 @@ app.use(express.static('public'));
 
 
 //start the server
-var server = app.listen(3000);
+var server = app.listen(3001, function () {
+
+ var host = server.address().address;
+ var port = server.address().port;
+
+ console.log('Registration web app listening at http://%s:%s', host, port);
+
+});
