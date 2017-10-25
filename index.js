15c320c2ee17cf7017cff3e1241d431d6b236184
Yamkela <owamjames1@gmail.com>
2017-10-25 12:29:29 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// index
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
--- models.js
+++ models.js
@@ -1,13 +1,20 @@
+// var mongoose = require('mongoose');
+// const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/regNumber";
+// mongoose.connect(mongoURL, function(err) {
+//   if (err) {
+//     console.log('Error Connecting to DB: ' + err);
+//   } else {
+//     console.log('connection to DB is successful');
+//   }
+// });
 var mongoose = require('mongoose');
-
-const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/regNumber";
-mongoose.connect(mongoURL, function(err) {
-  if (err) {
-    console.log('Error Connecting to DB: ' + err);
-  } else {
-    console.log('connection to DB is successful');
-  }
+const MongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/regNumber";
+console.log(MongoURL);
+mongoose.connect(MongoURL, {
+  useMongoClient: true
 });
+
+
 
 var RegNumber = mongoose.model('RegNumber', {
   RegNumber: String
